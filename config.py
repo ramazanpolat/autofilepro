b1ba967c87723db7e4e591bd9969e81182df38eb
Ramazan <ramazanpolat@gmail.com>
2019-05-20 07:45:27 +0300

    phase names updated

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 phases = [
     {
-        "name": "validate",
+        "name": "validate phase",
         "input_dir": "./input",
         "file_filter": "*.txt",
         "processor": "validate",
     },
     {
-        "name": "transform",
+        "name": "transform phase",
         "input_dir": "./input",
         "file_filter": "*.validate_ok",
         "processor": "transform",
